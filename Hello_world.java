7e7ac6016a6f74618f335f6238abad7fead49ef6
Anshul Kumar <54505790+anshulkumar1997@users.noreply.github.com>
2019-08-26 23:55:18 +0530

    Update Hello_world.java

--- Hello_world.java
+++ Hello_world.java
@@ -2,7 +2,7 @@
 public class Hello_world {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 		System.out.println("How you Doing");
 
 	}
