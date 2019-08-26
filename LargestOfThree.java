6b3db72a8edd0d67f83512daa3b9d91013b038dd
Anshul Kumar <54505790+anshulkumar1997@users.noreply.github.com>
2019-08-26 23:54:33 +0530

    Update LargestOfThree.java

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -2,7 +2,7 @@
 public class LargestOfThree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=44,b=24,c=42;
 		if(a>=b && a>=c)
 			System.out.println(a+" is Largest");
