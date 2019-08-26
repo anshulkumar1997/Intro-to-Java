cc3f6adfb3ad5cbbce6902aadbadc7b2e649189d
Anshul Kumar <54505790+anshulkumar1997@users.noreply.github.com>
2019-08-26 23:51:50 +0530

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,16 @@
+
+public class LargestOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=44,b=24,c=42;
+		if(a>=b && a>=c)
+			System.out.println(a+" is Largest");
+		else if(b>=c)
+			System.out.println(b+" is Largest");
+		else
+			System.out.println(c+" is Largest");
+
+	}
+
+}
