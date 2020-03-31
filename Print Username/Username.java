46e14fea1613a27a029dfc96899e1204e1aa99d7
Anshul Kumar <54505790+anshulkumar1997@users.noreply.github.com>
2020-04-01 01:58:50 +0530

    Username

---
+++ Print Username/Username.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Username{
+    public static void main (String[] args) {
+        String user;
+        Scanner name=new Scanner(System.in);
+        System.out.println("Enter the name:");
+        user=name.nextLine();
+        System.out.println("Welcome "+user);
+        
+    }
+}
